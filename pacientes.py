664dcc8827199a693ea82a1a54545b3f167bb031
Camila Suarez <camilaskemerer@gmail.com>
2022-06-30 21:35:33 -0300

    finnn

--- pacientes.py
+++ pacientes.py
@@ -1,4 +1,3 @@
-from ast import Num
 import json
 from datetime import datetime
 import yaml
--- pruebas.py
+++
@@ -1,8 +0,0 @@
-lista = []
-def enteros():
-    dato = int(input("Ingrese un número entero, cuando quiera terminar ingrese 'fin': "))
-    while dato != "fin":
-        lista.append(int(dato))
-        dato = input("Ingrese un número entero, cuando quiera terminar ingrese 'fin': ")
-    print(lista)
-enteros()
